586afb1c701fd13aa71e597d040ca128dad11974
Stella Elian <elian_stella@yahoo.com>
2026-01-30 18:49:10 +0100

    redirection du formulaire d ' Inscription, (chemin vers register.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@
 // database.php retourne $pdo ainsi on le récupère ici 
 $pdo = require_once '../config/database.php'; 
 require_once "../src/controllers/UserController.php";
+//controller de l'inscription 
+require_once "../src/controllers/RegisterController.php";
 // Show n'est pas statique alors je dois instancier l'objet pour l'utiliser 
-$controller = new UserController();
-$controller->show($pdo);
-$controller->register($pdo);
+$usercontroller = new UserController();
+$usercontroller->show($pdo);
+$registercontroller = new RegisterController();
+$registercontroller->showform();
---
+++ src/controllers/RegisterController.php
@@ -0,0 +1,7 @@
+<?php
+class RegisterController
+{
+    public function showForm(){
+        require_once "../src/views/register.php";
+    }
+}
