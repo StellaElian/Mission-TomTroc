4c0c412e5fc8e3b24faaec9cbd195faedebbac41
Stella Elian <elian_stella@yahoo.com>
2026-01-30 20:15:59 +0100

    login.php dans views de la page connexion

---
+++ src/views/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Page de Connexion</title>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <label>Pseudo</label>
+            <input type="text" name="pseudo">
+
+            <label>Mot de passe</label>
+            <input type="password" name="password">
+
+            <button type="submit">Se connecter</button>
