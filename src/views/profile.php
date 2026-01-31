fefd6c70e5e25570e578e4a0a6b2ea11c384c51e
Stella Elian <elian_stella@yahoo.com>
2026-01-31 16:40:06 +0100

    interface visuelle de mon compte

--- src/views/profile.php
+++ src/views/profile.php
@@ -6,7 +6,31 @@
     </head>
     <body>
         <h1>Mon compte</h1><br>
-        <p><?= $user['pseudo']; ?></p>
+        <div class="profile-main">
+            <div class="profile-left">
+            <img src="<?= $user['avatar'] ?? 'default-avatar.png' ?>" alt="Avatar" class="profile-avatar">
+            <a href="index.php?page=profile&edit=true" class="profile-edit">modifier</a>
+            <h2><?= $user['pseudo'] ?></h2>
+            <p>Membre depuis : <?= $user['created_at'] ?></p>
         
+        </div>
+
+        <div class="profile-right">
+            <h3>Vos informations personnelles</h3>
+            <form method="post" action="index.php?page=profile">
+                <label for="email">Email :</label>
+                <input type="email" name="email" id="email" value="<?= $user['email'] ?>">
+
+                <label for="pseudo">Pseudo :</label>
+                <input type="text" name="pseudo" id="pseudo" value="<?= $user['pseudo'] ?>">
+
+                <label for="password">Mot de passe :</label>
+                <input type="password" name="password" id="password">
+
+                <p>Bibliothèque</p>
+
+                <button type="submit">Enregistrer</button>
+            </form>
+        </div>
     </body>
 </html>
