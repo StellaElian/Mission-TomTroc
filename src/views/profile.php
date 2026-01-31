ae6d1f812cd743495791ae81260d0e681965e6f7
Stella Elian <elian_stella@yahoo.com>
2026-01-31 03:31:37 +0100

    profile.php fait

---
+++ src/views/profile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mon compte</title>
+    </head>
+    <body>
+        <h1>Mon compte</h1><br>
+        <p>Pseudo : <?= $user['pseudo']; ?></p>
+        <p>Email : <?= $use['email']; ?></p>
+    </body>
+</html>
