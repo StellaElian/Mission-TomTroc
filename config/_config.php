3d5ab1ad7a739c850f7fc5194df09a251ac5139b
Stella Elian <elian_stella@yahoo.com>
2026-02-02 05:18:11 +0100

    suppression chemins

--- config/_config.php
+++ config/_config.php
@@ -1,10 +1,6 @@
 <?php 
 // Sans session_start :le site oublie tout à chaque fois, présence obligatoire
 session_start();
-
-// Ici on met les constantes utiles et tout ce qui sert à configurer. 
-define('TEMPLATE_VIEW_PATH', '../views/'); // Le chemin vers les templates de vues.
-define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php'); // Le chemin vers le template principal.
 // les données de connexions à la bdd
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'tomtroc');
